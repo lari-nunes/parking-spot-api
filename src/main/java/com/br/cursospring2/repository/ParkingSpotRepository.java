e2506d78023db1fcfe592b3952ce2a4f97542a44
Larissa Nunes <larissanunes0904@gmail.com>
2023-12-11 23:23:48 -0300

    first commit

---
+++ src/main/java/com/br/cursospring2/repository/ParkingSpotRepository.java
@@ -0,0 +1,15 @@
+package com.br.cursospring2.repository;
+
+import com.br.cursospring2.model.ParkingSpotModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ParkingSpotRepository extends JpaRepository<ParkingSpotModel, UUID> {
+
+    boolean existsByLicensePlateCar(String licensePlateCar);
+    boolean existsByParkingSpotNumber(String parkingSpotNumber);
+    boolean existsByApartmentAndBlock(String apartment, String block);
+}
